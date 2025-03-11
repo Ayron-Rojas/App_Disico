206d75870eb3903857f85e8945728f64c1b4a67b
Ayron rojas <aironrojas@outlook.com>
2025-03-11 15:45:39 -0500

    rutas

---
+++ lib/routes/app_router.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '/pages/home_page.dart';
+import '/pages/registro_epp_page.dart';
+import '/pages/movimientos_page.dart';
+import '/pages/reportes_page.dart';
+
+class AppRouter {
+  // Definir instancia de rutas a usar en toda la app
+  static final router = GoRouter (
+    // ruta inicio
+    initialLocation: '/',
+    // demas rutas
+    routes: [
+      GoRoute(
+        path: '/',
+        name: 'home',
+        builder: (context, state) => HomePage(),
+      ),
+      GoRoute(
+        path: '/registro',
+        name: 'registro',
+        builder:(context, state) => RegistroEPPPage(),
+      ),
+      GoRoute(
+        path: '/movimientos',
+        name: 'movimientos',
+        builder:(context, state) => MovimientosPage(),
+      ),
+      GoRoute(
+        path: '/reportes',
+        name: 'reportes',
+        builder: (contex, state) => ReportesPage(),
+      )
+    ]
+  );
+}
